b2e4322e1090abe5f3b94efecc734a08de52eaa8
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-11 01:01:38 +0900

    サービスの実装

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { User } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateUserInput } from './dto/CreateUserInput';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(createUserInput: CreateUserInput): Promise<User> {
+    const { name, email, password } = createUserInput;
+    const hashedPassword = await bcrypt.hash(password, 10); // パスワードをハッシュ化
+
+    // ハッシュ化されたパスワードを使って新しいユーザーをデータベースに作成
+    const newUser = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword, // ハッシュ化したパスワードを保存
+      },
+    });
+
+    return newUser; // 新しく作成されたユーザーを返す
+  }
+}
